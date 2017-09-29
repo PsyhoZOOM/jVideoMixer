aa9a222c2400e334c3dd74c0a59b10d8e226bd26
PsyhoZOOM <psyhozoom@gmail.com>
2017-09-29 15:41:20 +0200

    First Commit

--- src/net/yuvideo/jvideomixer/Controller.java
+++ src/net/yuvideo/jvideomixer/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package net.yuvideo.jvideomixer;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -7,7 +7,10 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.SnapshotParameters;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.StackPane;
--- src/net/yuvideo/jvideomixer/Main.java
+++ src/net/yuvideo/jvideomixer/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package net.yuvideo.jvideomixer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
